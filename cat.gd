99234ffdba77f3d44f638486c02a853333abdb53
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-12-27 12:57:02 -0500

    cat can now attack using keyboard

--- cat.gd
+++ cat.gd
@@ -9,6 +9,9 @@ var u
 var r
 var l
 var idle
+var normal
+var skill
+var special
 
 func get_input():
 	dir_cnt = 0
@@ -28,11 +31,27 @@ func get_input():
 	if Input.is_action_pressed("ui_right"):
 		r = true
 		dir_cnt += 1
+	if !is_attacking():
+		if Input.is_action_just_pressed("normal_attack"):
+			normal = true
+			$AnimatedSprite2D.play("Scratch")
+			idle = false
+		if Input.is_action_just_pressed("skill"):
+			skill = true
+			$AnimatedSprite2D.play("Tail Whip")
+			idle = false
+		if Input.is_action_just_pressed("special_skill"):
+			special = true
+			$AnimatedSprite2D.play("Fire")
+			idle = false
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
-	if dir_cnt > 2 or dir_cnt == 0 or (l == true and r == true) or (u == true and d == true):
+	handle_attack_animation()
+	print($AnimatedSprite2D.animation)
+	if is_attacking():
+		idle = false
+	elif dir_cnt > 2 or dir_cnt == 0 or (l == true and r == true) or (u == true and d == true):
 		idle = true
 	else:
 		idle = false
@@ -40,7 +59,7 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play('Idle')
 		velocity.x = 0
 		velocity.y = 0
-	else:
+	elif !is_attacking():
 		if r == true:
 			if flip:
 				scale.x = -1
@@ -69,3 +88,24 @@ func _physics_process(delta):
 				velocity.y = SPEED * sqrt(0.5)
 		$AnimatedSprite2D.play("Walk")
 	move_and_slide()
+
+func  _ready():
+	normal = false
+	skill = false
+	special = false
+
+func is_attacking():
+	if normal or skill or special:
+		return true
+	return false
+	
+func handle_attack_animation():
+	if $AnimatedSprite2D.frame_progress < 1.0:
+		return
+	if $AnimatedSprite2D.animation == "Scratch":
+		normal = false
+	elif $AnimatedSprite2D.animation == "Tail Whip":
+		skill = false
+	elif $AnimatedSprite2D.animation == "Fire":
+		special = false
+	$AnimatedSprite2D.play("Idle")
