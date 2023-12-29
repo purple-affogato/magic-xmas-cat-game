999419efefc966cb3347c2e2df2a4d680a0b3297
daynaaaa <danya.eggy@gmail.com>
2023-12-29 01:07:28 -0500

    credits

--- ParallaxBackground.gd
+++ ParallaxBackground.gd
@@ -3,3 +3,7 @@ extends ParallaxBackground
 
 func _process(delta):
 	scroll_base_offset += Vector2(50,0)*delta
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("menu.tscn")
--- menu.gd
+++ menu.gd
@@ -9,3 +9,7 @@ func _on_play_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("credits.tscn")
