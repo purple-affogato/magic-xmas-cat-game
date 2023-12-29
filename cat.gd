82ce48dec5095c547441014d3be74fca1d60abe6
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-12-29 02:29:19 -0500

    added fire skill to future lvl

--- cat.gd
+++ cat.gd
@@ -48,6 +48,11 @@ func get_input():
 			$AnimatedSprite2D.play("Water")
 			$Water/AOE.disabled = false
 			idle = false
+		if Input.is_action_just_pressed("fire_skill") and get_parent().name == "FutureLevel":
+			fire = true
+			$AnimatedSprite2D.play("Fire")
+			$Fire/AOE.disabled = false
+			idle = false
 
 func _process(_delta):
 	get_input()
@@ -116,50 +121,37 @@ func handle_attack_animation():
 	elif $AnimatedSprite2D.animation == "Water":
 		water = false
 		$Water/AOE.disabled = true
+	elif $AnimatedSprite2D.animation == "Fire":
+		fire = false
+		$Fire/AOE.disabled = true
 	$AnimatedSprite2D.play("Idle")
 
-
-func _on_scratch_body_entered(body):
-	print(body.name)
-	if body.name.contains("Fox") or body.name.contains("Bird"):
-		body.hp -= 1
+func add_dmg_number(num, body):
+	if body.name.contains("Fox") or body.name.contains("Bird") or body.name.contains("Dog"):
+		body.hp -= num
 		var dmg = preload("res://damage.tscn").instantiate()
-		body.add_child(dmg)
-		dmg.get_node("Sprite2D").frame = 1
+		dmg.get_node("Sprite2D").frame = num
 		dmg.position = body.get_node("DMGPosition").position
 		dmg.add_to_group("dmg")
 		body.get_node("OuchTimer").start()
 		if body.flip:
 			dmg.scale.x = -dmg.scale.x
+		if len(body.get_nodes_in_group("dmg")) > 0:
+			body.get_nodes_in_group("dmg").clear()
+		body.add_child(dmg)
+
+func _on_scratch_body_entered(body):
+	add_dmg_number(1, body)
 
 func _on_ouch_timer_timeout():
 	for i in get_tree().get_nodes_in_group("dmg"):
 		i.queue_free()
 
-
 func _on_whip_body_entered(body):
-	#print(body.get_name())
-	if body.name.contains("Fox") or body.name.contains("Bird"):
-		body.hp -= 2
-		var dmg = preload("res://damage.tscn").instantiate()
-		body.add_child(dmg)
-		dmg.get_node("Sprite2D").frame = 2
-		dmg.position = body.get_node("DMGPosition").position
-		dmg.add_to_group("dmg")
-		body.get_node("OuchTimer").start()
-		if body.flip:
-			dmg.scale.x = -dmg.scale.x
-
+	add_dmg_number(2, body)
 
 func _on_water_body_entered(body):
-	#print(body.name)
-	if body.name.contains("Fox") or body.name.contains("Bird"):
-		body.hp -= 3
-		var dmg = preload("res://damage.tscn").instantiate()
-		body.add_child(dmg)
-		dmg.get_node("Sprite2D").frame = 3
-		dmg.position = body.get_node("DMGPosition").position
-		dmg.add_to_group("dmg")
-		body.get_node("OuchTimer").start()
-		if body.flip:
-			dmg.scale.x = -dmg.scale.x
+	add_dmg_number(3, body)
+
+func _on_fire_body_entered(body):
+	add_dmg_number(3, body)
